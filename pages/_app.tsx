a19762eef000cec77b4894648623b14c29ed0e60
Marc <91346941+md1440@users.noreply.github.com>
2022-02-13 17:34:29 +0100

    add Layout to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout Component={Component} pageProps={pageProps} />
+      
+  );
 }
 
 export default MyApp;
