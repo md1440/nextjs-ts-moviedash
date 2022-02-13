20e3a0bb6364e7ee3b7a8f94fd074ab10cefeca6
Marc <91346941+md1440@users.noreply.github.com>
2022-02-13 16:02:09 +0100

    add Api - movieApi useMovieApi - axios interceptor

--- src/utils/Api.tsx
+++ src/utils/Api.tsx
@@ -1,18 +1,26 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-self-assign */
 import axios, { AxiosResponse, Method } from 'axios';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Movie } from '../types/types';
 
 // *** Check if Movie or Movie[] and transform data property
 axios.interceptors.response.use(
   (res: AxiosResponse) => {
     if (res && res.data.data.movie) {
       res.data = res.data.data.movie;
+      if (!res.data.movie.poster) {
+        res.data.movie.poster = '/public/images/2084555.jpg'
+      }
       return res;
     }
 
     if (res && res.data.data.movies) {
       res.data = res.data.data.movies;
-      return res;
+      res.data = res.data.filter((movie: Movie) => movie.poster !== undefined);
+      return res
     }
+
     return res;
   },
   (error) => Promise.reject(error),
@@ -21,7 +29,7 @@ axios.interceptors.response.use(
 export function movieApi<T>(
   method: Method,
   path: string,
-  callback?: (data: T) => void,
+  callback: (data: T) => void,
   data = {},
 ): void {
   const baseUrl = 'https://moviedb-rest-api.herokuapp.com/api/v1/movies';
@@ -31,8 +39,7 @@ export function movieApi<T>(
     data,
   })
     .then((res: AxiosResponse<T>): void => {
-      if (callback) callback(res.data);
-      res.data;
+      callback(res.data);
     })
     .catch();
 }
