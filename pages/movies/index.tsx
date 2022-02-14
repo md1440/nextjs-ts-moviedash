b994408c6266df22913fec90d009daf65b4bde09
Marc <91346941+md1440@users.noreply.github.com>
2022-02-14 09:31:11 +0100

    add MovieList

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import React, { ReactElement } from 'react';
+import { MdStars } from 'react-icons/md';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { Movie } from '../../src/types/types';
 import { useMovieApi } from '../../src/utils/Api';
@@ -22,18 +25,44 @@ function MovieList(): ReactElement {
         <h1 className="mt-12 text-center text-3xl font-extrabold">
           List of all Movies
         </h1>
-        <ul>
+        <div className="mt-8 flex flex-row flex-wrap justify-around gap-7">
           {movies.map(
             (movie: Movie): ReactElement => (
               <Link key={movie._id} href={`/movies/${movie._id}`} passHref>
-                <li className="px-3 py-2">{movie.title}</li>
+                <div className="w-56 cursor-pointer rounded-lg bg-indigo-100 shadow-lg">
+                  <div className="relative my-2 h-60 rounded-t-lg">
+                    {movie.poster && (
+                      <Image
+                        src={movie.poster}
+                        objectFit="contain"
+                        layout="fill"
+                      />
+                    )}
+                  </div>
+                  <div className="rounded-b-lg bg-gray-100 px-3 py-2">
+                    <h2 className="text-sm font-semibold">{movie.title}</h2>
+                    <div className="flex flex-row items-center justify-between pt-1">
+                      <div className="flex flex-row items-center mt-1">
+                        <p className="text-sm text-yellow-500 mr-1">
+                          <MdStars />
+                        </p>
+                        <p className="text-sm font-semibold">
+                          {movie.imdb.rating}
+                        </p>
+                      </div>
+                      <p className="text-sm font-medium ">{movie.year}</p>
+                    </div>
+                    <p className="text-sm font-regular italic">
+                      {movie.genres.join(', ')}
+                    </p>
+                  </div>
+                </div>
               </Link>
             ),
           )}
-        </ul>
+        </div>
       </main>
-
-      <footer className="flex"></footer>
+      <footer className="flex" />
     </div>
   );
 }
