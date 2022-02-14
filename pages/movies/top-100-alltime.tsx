35141a435fc267a2403aa207a7236547e24e0cac
Marc <91346941+md1440@users.noreply.github.com>
2022-02-14 10:42:10 +0100

    add top100-alltime functionality

--- pages/movies/top-100-alltime.tsx
+++ pages/movies/top-100-alltime.tsx
@@ -1,56 +1,27 @@
-import axios from 'axios';
+/* eslint-disable no-underscore-dangle */
+import Head from 'next/head';
 import React, { ReactElement } from 'react';
-import useSWR from 'swr';
+import LoadingSpinner from '../../components/LoadingSpinner';
+import MovieCardList from '../../components/MovieCardList';
 import { Movie } from '../../src/types/types';
+import { useMovieApi } from '../../src/utils/Api';
 
-function Top100Alltime() {
-  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-  const { data, error } = useSWR(
-    'https://moviedb-rest-api.herokuapp.com/api/v1/movies/top-100-alltime',
-    fetcher,
-  );
+function Top100Alltime(): ReactElement {
+  const [movies, setMovies] = useMovieApi<Movie[]>('/top-100-alltime');
 
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
-
-  const collection: Movie[] = data.data.movies;
-  console.log(collection);
+  if (!movies) return <LoadingSpinner />;
 
   return (
-    <div className="flex">
-      allmovies
-      <ul>
-        {collection.map(
-          (movie: Movie): ReactElement => (
-            <li key={movie._id}>
-              {movie.title}
-              <div className="p-3 pl-4">
-                <div className="mt-3 text-xl font-bold tracking-wide text-gray-900">
-                  {movie.title}
-                </div>
-                <div className="mt-[0.5px] text-lg italic text-gray-800">
-                  {movie.plot}
-                </div>
-                <div className="mt-5 text-base text-gray-500">
-                  {/* {movie.cast.map((cast: string) => (
-                    <span
-                      key={cast}
-                      className="pr-2 after:content-[','] after:last-of-type:content-['']"
-                    >
-                      {cast}
-                    </span>
-                  ))} */}
-                  <p className="pt-1">
-                    ISBN:
-                    <span className="pl-1" />
-                    <span className="tracking-wide">{movie.directors}</span>
-                  </p>
-                </div>
-              </div>
-            </li>
-          ),
-        )}
-      </ul>
+    <div className="font-Poppins">
+      <Head>
+        <title>List of all Movies</title>
+        <meta name="description" content="Top 100 rated Movies of Alltime" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="mt-12 text-center text-5xl font-black tracking-wider text-indigo-500">
+        Top 100 Movies of Alltime
+      </h1>
+      <MovieCardList movies={movies} />
     </div>
   );
 }
