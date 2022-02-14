121a9862084a41ea842868f8a5cf1b5dd221f76d
Marc <91346941+md1440@users.noreply.github.com>
2022-02-14 10:40:48 +0100

    cleanup, modify

--- components/MovieCardItem.tsx
+++ components/MovieCardItem.tsx
@@ -11,25 +11,25 @@ interface Props {
 function MovieCardItem({ movie }: Props): ReactElement {
   return (
     <Link key={movie._id} href={`/movies/${movie._id}`} passHref>
-      <div className="w-56 cursor-pointer rounded-lg bg-indigo-100 shadow-lg">
+      <div className="w-60 cursor-pointer rounded-lg bg-indigo-100 shadow-lg">
         <div className="relative my-2 h-60 rounded-t-lg">
           {movie.poster && (
             <Image src={movie.poster} objectFit="contain" layout="fill" />
           )}
         </div>
         <div className="rounded-b-lg bg-gray-100 px-3 py-2">
-          <h2 className="text-sm font-semibold">{movie.title}</h2>
+          <h2 className="text-sm font-medium ">{movie.title.slice(0, 25)}</h2>
           <div className="flex flex-row items-center justify-between pt-1">
             <div className="mt-1 flex flex-row items-center">
               <p className="mr-1 text-sm text-yellow-500">
                 <MdStars />
               </p>
-              <p className="text-sm font-semibold">{movie.imdb.rating}</p>
+              <p className="text-sm font-medium">{movie.imdb.rating}</p>
             </div>
-            <p className="text-sm font-medium ">{movie.year}</p>
+            <p className="text-sm font-regular ">{movie.year}</p>
           </div>
-          <p className="font-regular text-sm italic">
-            {movie.genres.join(', ')}
+          <p className="font-light text-sm italic">
+            {movie.genres.slice(0, 2).join(', ')}
           </p>
         </div>
       </div>
