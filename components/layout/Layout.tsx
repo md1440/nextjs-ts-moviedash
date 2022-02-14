7cf083092c2072524ecf040b7569c69f9cd73002
Marc <91346941+md1440@users.noreply.github.com>
2022-02-14 10:41:22 +0100

    factor out NavBar in own component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { NextComponentType, NextPageContext } from 'next/types';
 import React, { ReactElement } from 'react';
+import NavBar from './NavBar';
 
 interface Props {
   Component: NextComponentType<NextPageContext, any, {}>;
@@ -11,30 +12,16 @@ interface Props {
 
 function Layout({ Component, pageProps }: Props): ReactElement {
   return (
-    <div className="bg-gray-100">
-      <div className="max-w-8xl bg container mx-auto min-h-screen px-24 pb-12 font-Poppins">
-        <header className="">
-          <nav>
-            <ul className="fontmedium flex flex-row justify-between bg-indigo-400 px-3 text-lg uppercase tracking-wide text-white">
-              <Link href="/" passHref>
-                <li className="cursor-pointer px-3 py-4">Home</li>
-              </Link>
-              <Link href="/movies" passHref>
-                <li className="cursor-pointer px-3 py-4">All Movies</li>
-              </Link>
-              <Link href="/movies/add" passHref>
-                <li className="cursor-pointer px-3 py-4">Create Movie</li>
-              </Link>
-              <li className="cursor-pointer px-3 py-4">Top 100 Movies</li>
-              <li className="cursor-pointer px-3 py-4">Random Movie</li>
-              <li className="cursor-pointer px-3 py-4">Statistics</li>
-            </ul>
-          </nav>
-        </header>
+    <div className="bg-gray-100 font-Poppins">
+      <header className="">
+        <NavBar />
+      </header>
+      <div className="max-w-8xl bg container mx-auto min-h-screen px-24 pb-12 ">
         <div>
           <Component {...pageProps} />
         </div>
       </div>
+      <footer className="h-40 bg-indigo-400" />
     </div>
   );
 }
