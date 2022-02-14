e043d5fbe4c20fcf33920d5e6ff4eb509a9e3a6e
Marc <91346941+md1440@users.noreply.github.com>
2022-02-14 10:42:40 +0100

    factor out MovieCardlist into own component

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import React, { ReactElement } from 'react';
-import { MdStars } from 'react-icons/md';
 import LoadingSpinner from '../../components/LoadingSpinner';
+import MovieCardList from '../../components/MovieCardList';
 import { Movie } from '../../src/types/types';
 import { useMovieApi } from '../../src/utils/Api';
 
@@ -20,49 +18,10 @@ function MovieList(): ReactElement {
         <meta name="description" content="A list of all Movies" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="container mx-auto">
-        <h1 className="mt-12 text-center text-3xl font-extrabold">
-          List of all Movies
-        </h1>
-        <div className="mt-8 flex flex-row flex-wrap justify-around gap-7">
-          {movies.map(
-            (movie: Movie): ReactElement => (
-              <Link key={movie._id} href={`/movies/${movie._id}`} passHref>
-                <div className="w-56 cursor-pointer rounded-lg bg-indigo-100 shadow-lg">
-                  <div className="relative my-2 h-60 rounded-t-lg">
-                    {movie.poster && (
-                      <Image
-                        src={movie.poster}
-                        objectFit="contain"
-                        layout="fill"
-                      />
-                    )}
-                  </div>
-                  <div className="rounded-b-lg bg-gray-100 px-3 py-2">
-                    <h2 className="text-sm font-semibold">{movie.title}</h2>
-                    <div className="flex flex-row items-center justify-between pt-1">
-                      <div className="flex flex-row items-center mt-1">
-                        <p className="text-sm text-yellow-500 mr-1">
-                          <MdStars />
-                        </p>
-                        <p className="text-sm font-semibold">
-                          {movie.imdb.rating}
-                        </p>
-                      </div>
-                      <p className="text-sm font-medium ">{movie.year}</p>
-                    </div>
-                    <p className="text-sm font-regular italic">
-                      {movie.genres.join(', ')}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            ),
-          )}
-        </div>
-      </main>
-      <footer className="flex" />
+      <h1 className="mt-12 text-center text-5xl font-black tracking-wider text-indigo-500">
+        List of all Movies
+      </h1>
+      <MovieCardList movies={movies} />
     </div>
   );
 }
