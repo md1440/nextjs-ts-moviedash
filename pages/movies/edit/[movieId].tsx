2f77346960025362e8780fe57c04f2d04f34445a
Marc <91346941+md1440@users.noreply.github.com>
2022-02-18 10:47:02 +0100

    bugfix

--- pages/movies/edit/[movieId].tsx
+++ pages/movies/edit/[movieId].tsx
@@ -13,7 +13,6 @@ interface Props {
 
 function MovieEdit({query}: Props): ReactElement {
   const { back, asPath, push, replace } = useRouter();
-  console.log('MovieEdit', query)
 
   // *** Get movie api call, useState
   const [movie] = useMovieApi<Movie>(`/${query.movieid}`);
